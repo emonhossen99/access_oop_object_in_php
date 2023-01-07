26932aa176f060ba156fa923cf2ef0781cdf50ab
ahamedemon99 <mdxhamedemon@gmail.com>
2023-01-07 11:03:06 +0600

    added obect

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+include('oop.php');
+
+$blue_print = new Person();
+$name = $blue_print->name;
+$age = $blue_print->age;
+$hobby = $blue_print->hobby;
+$work = $blue_print->work;
+echo $name.$age.$hobby.$work;
+
+?>
---
+++ oop.php
@@ -0,0 +1,11 @@
+<?php
+
+class Person {
+    var $name = 'Emon';
+    var $age = '22';
+    var $hobby = 'coding';
+    var $work = 'student';
+}
+
+
+?>
